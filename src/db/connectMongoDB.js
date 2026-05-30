5d97091cabfefc9a4f4a96de5ab6eee0e3c4ac75
Samsahaha <motornijsemen@gmail.com>
2026-05-30 15:02:03 +0300

    1

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -1,8 +1,13 @@
+import dns from 'node:dns';
 import mongoose from 'mongoose';
 
 export const connectMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    dns.setServers(['8.8.8.8', '8.8.4.4']);
+
+    await mongoose.connect(process.env.MONGO_URL, {
+      family: 4,
+    });
     console.log('✅ MongoDB connection established successfully');
   } catch (error) {
     console.error('Failed to connect to MongoDB:', error.message);
